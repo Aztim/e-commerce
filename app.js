83c98f62e331ebf7be4c39b1b557b6c399956ee1
Tim-82 <barboza-82@mail.ru>
2020-09-01 23:37:45 +0300

    Added request to get products data

--- app.js
+++ app.js
@@ -12,5 +12,28 @@ const productsDom = document.querySelector('.products-center');
 let car = [];
 
 class Products {
-  
+  async getProducts() {
+    try {
+      let result = await fetch('products.json');
+      return result;
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
+
+class UI {
+
+}
+
+class Storage {
+
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const ui = new UI()
+  const products = new Products();
+
+  products.getProducts().then(data => console.log(data))
+})
+
