a9998a768728e01ba6400cf952b7bfb8efc3f0e7
Tim-82 <barboza-82@mail.ru>
2020-09-04 23:29:23 +0300

    create local.Storage and add axios

--- app.js
+++ app.js
@@ -58,15 +58,20 @@ class UI {
      productsDom.iierHTML = result;
    }
 }
-
+/* 
+local storage */
 class Storage {
-
+  static saveProducts(products) {
+    localStorage.setItem("products") 
+  }
 }
 
 document.addEventListener('DOMContentLoaded', ()=> {
   const ui = new UI()
   const products = new Products();
 
-  products.getProducts().then(products => ui.displayProducts(products))
+  products.getProducts().then(products => { 
+  ui.displayProducts(products))
+  Storage.saveProducts(products);
 })
 
